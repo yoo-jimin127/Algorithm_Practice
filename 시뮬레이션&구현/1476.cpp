0364c9ca4c65a61325032ec1aa82671865638401
유지민 <66112716+yoo-jimin127@users.noreply.github.com>
2021-12-29 00:51:34 +0900

    Create 1476.cpp

---
+++ 시뮬레이션&구현/1476.cpp
@@ -0,0 +1,31 @@
+//백준 1476 날짜 계산
+#include <stdio.h>
+
+#pragma warning (disable:4996)
+
+int main(void) {
+	int E, S, M;
+	int yearE, yearS, yearM, cnt = 1;
+
+	scanf("%d %d %d", &E, &S, &M);
+
+	yearE = yearS = yearM = 1;
+
+	while (E != yearE || S != yearS || M != yearM) {
+		//printf("%d %d %d", yearE, yearS, yearM);
+
+		if (yearE < 15) yearE++;
+		else yearE = 1;
+
+		if (yearS < 28) yearS++;
+		else yearS = 1;
+
+		if (yearM < 19) yearM++;
+		else yearM = 1;
+		
+		cnt++;
+	}
+
+	printf("%d", cnt);
+	return 0;
+}
